e9ca53af9cc42b7634a94c804d42a2fd5c8a1e74
xiaodai <2434147365@qq.com>
2020-05-13 14:04:47 +0800

    事件、以及鼠标事件

--- Vue_study/app.js
+++ Vue_study/app.js
@@ -2,15 +2,22 @@
 new Vue({
 	el:"#vue-app",    // el ：element 需要获取到的元素、一定是html中的跟他容器元素
 	data:{			 // data：用于数据的存储
-		name:"米斯特戴",
 		age:18,
-		job:"web开发",
-		website:"http://erge1998.cn",
-		websiteTag:'<a href="http://erge1998.cn">这是一个跳转标签</a>'
+		x:0,
+		y:0
 	},
 	methods:{
-		greet:function(time){
-			return 'Good '+ time+" "+ this.name+' !' ;
+		add:function(inc){
+			this.age += inc;
 		},
+		subtract:function(dec){
+			this.age -= dec;
+		},
+		updateXY:function(event){
+			// console.log(event)
+			this.x = event.offsetX;
+			this.y = event.offsetY;
+			return this.y
+		}
 	}
 });
